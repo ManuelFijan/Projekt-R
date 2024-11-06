9df7a0f06f8b08d9c7476fcfd7c2eb3989a64255
ManuelFijan <manuel.fijan@fer.unizg.hr>
2024-11-07 00:12:23 +0100

    napravljene stranice uvjeti poslovanja, izjava o privatnosti, kontakt, error

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -55,9 +55,11 @@ function LoginPage() {
                             </div>
                         </div>
 
+                        {//OVO TREBA POPRAVITI PRIJE PREDAJE DA BUDE type="submit"
+                        }
                         <div>
                             <button
-                                type="submit"
+                                type="button"
                                 className="flex w-full justify-center rounded-md bg-orange-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                             >
                                 <Link to={"/homepage"}>Prijava</Link>
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -24,7 +24,7 @@ function RegisterPage() {
                                 <input
                                     id="ime"
                                     name="ime"
-                                    type="email"
+                                    type="text"
                                     required
                                     autoComplete="email"
                                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-300 sm:text-sm sm:leading-6"
@@ -39,7 +39,7 @@ function RegisterPage() {
                                 <input
                                     id="email"
                                     name="email"
-                                    type="email"
+                                    type="text"
                                     required
                                     autoComplete="email"
                                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-300 sm:text-sm sm:leading-6"
